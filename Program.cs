c441a50614f2c3cb323ef3aaa126a4ef13c96252
Diptichougule04 <144772981+Diptichougule04@users.noreply.github.com>
2024-10-13 14:45:20 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitHubFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to github....!");
+        }
+    }
+}
